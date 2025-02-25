d52499500f9efb9639a4b793e265501897ecb3dd
sombathdanet <sombatdanet@gmail.com>
2025-02-25 14:22:51 +0700

    remove android service

--- android/src/main/kotlin/yukams/app/background_locator_2/provider/LocationParserUtil.kt
+++ android/src/main/kotlin/yukams/app/background_locator_2/provider/LocationParserUtil.kt
@@ -1,36 +1,13 @@
 package yukams.app.background_locator_2.provider
 
-import android.location.Location
 import android.os.Build
 import com.google.android.gms.location.LocationResult
 import yukams.app.background_locator_2.Keys
 
 class LocationParserUtil {
     companion object {
-        fun getLocationMapFromLocation(location: Location): HashMap<String, Any?> {
-            var speedAccuracy = 0f
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                speedAccuracy = location.speedAccuracyMetersPerSecond
-            }
-            val kotlinNull: HashMap<String, Any?>?;
 
-
-            kotlinNull = hashMapOf(
-                Keys.ARG_IS_MOCKED to false,
-                Keys.ARG_LATITUDE to location.latitude,
-                Keys.ARG_LONGITUDE to location.longitude,
-                Keys.ARG_ACCURACY to location.accuracy,
-                Keys.ARG_ALTITUDE to location.altitude,
-                Keys.ARG_SPEED to location.speed,
-                Keys.ARG_SPEED_ACCURACY to speedAccuracy,
-                Keys.ARG_HEADING to location.bearing,
-                Keys.ARG_TIME to location.time.toDouble(),
-                Keys.ARG_PROVIDER to location.provider,
-            )
-            return kotlinNull;
-        }
-
-        fun getLocationMapFromLocation(location: LocationResult?): java.util.HashMap<String, out Any?>? {
+        fun getLocationMapFromLocation(location: LocationResult?): HashMap<String, Any?>? {
             val firstLocation = location?.lastLocation ?: return null;
 
             var speedAccuracy = 0f
--- android/src/main/kotlin/yukams/app/background_locator_2/provider/LocationUpdateListener.kt
+++ android/src/main/kotlin/yukams/app/background_locator_2/provider/LocationUpdateListener.kt
@@ -1,7 +1,5 @@
 package yukams.app.background_locator_2.provider
 
-import java.util.HashMap
-
 interface LocationUpdateListener {
-    fun onLocationUpdated(location: HashMap<String, out Any?>?)
+    fun onLocationUpdated(location: HashMap<String, Any>?)
 }
